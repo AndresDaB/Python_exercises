82e991a7feda17d91ed12764854d65123de2ca1a
Andres Garcia <33291315+AndresDaB@users.noreply.github.com>
2025-09-18 20:26:29 -0600

    Update biggestnumber_in_array.py

--- biggestnumber_in_array.py
+++ biggestnumber_in_array.py
@@ -6,5 +6,5 @@ def largest_element(arr, n):
 if __name__ == "__main__":
     arr = [1, 20, 50, 78, 2, 5, 100]
     k = 3
-    res = largest_element(arr, n)
+    res = largest_element(arr, k)
     print(' '.join(map(str, res)))
